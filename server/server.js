af1da73fbb0c2a8bbd0e650d27852c0b7aa61094
Gaurav Vadnere <gauravvadnere789@gmail.com>
2024-07-11 16:43:06 +0530

    Configured server and application

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+    res.send({ message: 'Nothing here!' });
+})
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello from TMS API!' });
+})
+
 
 app.listen(port, () => {
     console.log('Server listening at port:', port);
