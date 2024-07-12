8ab177e90188d744901e0f30e0f6de6fe7077951
Gaurav Vadnere <gauravvadnere789@gmail.com>
2024-07-12 16:41:12 +0530

    Created API paths for CRUD operations

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private apiUrl = `${environment.apiUrl}/items`;
+
   constructor(private http: HttpClient) {
+  }
+
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getTasks() {
+    return this.http.get(this.apiUrl, { observe: 'response' });
+  }
+
+  addTask(item: any) {
+    return this.http.post(this.apiUrl, item);
+  }
+
+  addItem(item: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, item);
+  }
+
+  updateItem(id: string, item: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${id}`, item);
+  }
+
+  deleteItem(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
